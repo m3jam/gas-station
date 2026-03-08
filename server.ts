fd0240a017c0ba36603f29e707191301e3b8c7f4
m3jam <info.mohamed35@gmail.com>
2026-03-08 09:55:55 +0300

    fix: correct dist path in production

--- server.ts
+++ server.ts
@@ -840,9 +840,9 @@ async function startServer() {
       app.use(vite.middlewares);
     } else {
       console.log("Starting in production mode...");
-      app.use(express.static(path.join(__dirname, "dist")));
+      app.use(express.static(path.join(__dirname, ".." , "dist")));
       app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "dist", "index.html"));
+        res.sendFile(path.join(__dirname, ".." , "dist", "index.html"));
       });
     }
 
